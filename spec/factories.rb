0f50296217afee2e88d0f41c7c1ee806b7fec6d4
David <1337dallen@gmail.com>
2019-06-23 14:25:18 +1000

    Add and setup rspec and factory_bot

---
+++ spec/factories.rb
@@ -0,0 +1,15 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    sequence(:email) { |n| "user_##{n}@example.com" }
+    phone_number { Faker::PhoneNumber.phone_number }
+  end
+
+  factory :post do
+    association :user
+    title { Faker::Hacker.say_something_smart }
+    body { Faker::Lorem.sentences(rand(10..35)).join(' ') }
+  end
+end

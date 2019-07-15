009cc4968b721b50b98b89ae873b08e3430a4ac2
David <1337dallen@gmail.com>
2019-07-15 21:20:23 +1000

    Add index to email column of users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_12_065157) do
+ActiveRecord::Schema.define(version: 2019_07_15_111508) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_12_065157) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
